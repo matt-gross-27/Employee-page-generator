a7d4ac61c43bb5fbb19db0fc11f7353b5c90a3ef
Matt Gross <mbgross111@gmail.com>
2021-02-18 12:26:17 -0700

    add tested Employee constructor

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,30 @@
+const Employee = require('../lib/Employee');
+
+const employee = new Employee('Fran','000394','fran91@myCompany.com')
+
+test('creates an employee object with 3 properties (name, id, email)', () => {
+  expect(employee).toEqual(expect.any(Object));
+  expect(employee.name).toBe('Fran');
+  expect(employee.id).toBe('000394');
+  expect(employee.email).toBe('fran91@myCompany.com');
+});
+
+test('getName() method to return the name property', ()=> {
+  expect(employee.getName()).toBe('Fran');
+});
+
+test('getId() method to return the id property', ()=> {
+  expect(employee.getId()).toBe('000394');
+});
+
+test('getEmail() method to return the email property', ()=> {
+  expect(employee.getEmail()).toBe('fran91@myCompany.com');
+});
+
+test('getRole() method to return the constructor name (which is named after role)', ()=> {
+  expect(employee.getRole()).toBe('Employee');
+});
+
+// getEmail()
+
+// getRole() // Returns 'Employee'
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,4 @@
+// placeholder until I start actually testing this Object
+test('1 = 1', () => {
+  expect(1).toBe(1);
+})
