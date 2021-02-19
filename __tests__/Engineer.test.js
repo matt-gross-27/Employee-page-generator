bb4f48df68d48a5b59ed695606ab1d9068887f76
Matt Gross <mbgross111@gmail.com>
2021-02-19 12:20:17 -0700

    add tested Engineer constructor that extends Employee constructor

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,4 +1,31 @@
-// placeholder until I start actually testing this Object
-test('1 = 1', () => {
-  expect(1).toBe(1);
-})
+const Engineer = require('../lib/Engineer');
+
+const engineer = new Engineer('Fran','000394','fran91@myCompany.com','python-fran-lib')
+
+test('creates an employee object with 3 properties (name, id, email, github)', () => {
+  expect(engineer).toEqual(expect.any(Object));
+  expect(engineer.name).toBe('Fran');
+  expect(engineer.id).toBe('000394');
+  expect(engineer.email).toBe('fran91@myCompany.com');
+  expect(engineer.github).toBe('python-fran-lib');
+});
+
+test('getName() method to return the name property', ()=> {
+  expect(engineer.getName()).toBe('Fran');
+});
+
+test('getId() method to return the id property', ()=> {
+  expect(engineer.getId()).toBe('000394');
+});
+
+test('getEmail() method to return the email property', ()=> {
+  expect(engineer.getEmail()).toBe('fran91@myCompany.com');
+});
+
+test('getRole() method to return the constructor name (which is named after role)', ()=> {
+  expect(engineer.getRole()).toBe('Engineer');
+});
+
+test('getGithub() method to return the github property of the object', ()=> {
+  expect(engineer.getGithub()).toBe('python-fran-lib');
+});
